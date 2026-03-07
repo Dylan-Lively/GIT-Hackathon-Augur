d592f89ef2372538d5d208ba66fead6c4d506ad9
Dylan-Lively <dyl.lively@gmail.com>
2026-03-07 13:23:03 -0800

    Setup Backend

---
+++ backend/src/test/java/com/dylanlively/Augur/AugurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylanlively.Augur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
