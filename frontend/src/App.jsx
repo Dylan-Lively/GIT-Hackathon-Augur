61e772bf84da2a897c9773a3ebc6d0ce80cdf120
Denton Manders <dentonmanders@gmail.com>
2026-03-07 13:17:45 -0800

    moved to frontend + moves in setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./NavBar";
 import MainPage from "./MainPage";
 import Setup from "./Setup";
+import Settings from "./Settings";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/setup" element={<Setup />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
