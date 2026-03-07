bd44b0b46746f08c392de48471f7259862dad9b8
Dylan-Lively <dyl.lively@gmail.com>
2026-03-07 15:18:48 -0800

    testing button ftfh

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -1,6 +1,26 @@
 
 export default function MainPage() {
-    return(
-        <></>
+    async function runSimulation() {
+        const response = await fetch("http://localhost:8080/simulate", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ preset: "coffee_shop" })
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+    return (
+       
+        <>
+        <h1 className="text-3xl font-bold">hello world</h1>
+<button 
+  onClick={runSimulation}
+  className="bg-blue-500 text-white px-4 py-2 rounded"
+>
+  Test Connection
+</button>
+
+        </>
     )
 }
